b9d32df9537a5401d56672a1dc9408f77e376cb3
Alexandr <kandarash@live.ru>
2014-03-13 22:28:44 +0400

    Before changing sqrt method (preparing for parallel version)

--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<stdio.h>
+#include<time.h>
 #include"sqrtMethod.h"
 #include"conjugateGradientMethod.h"
 
@@ -8,6 +9,7 @@ using namespace std;
 int main()
 {
 	int size;
+	unsigned int time_before, time_after;
 	cout.precision(4);
 	FILE* f=freopen("helpful/input_symmetrical_1000", "r", stdin);
 	cin >> size;
@@ -22,19 +24,22 @@ int main()
 	for(int i=0; i<size; i++)
 		for(int j=0; j<size+1; j++)
 			cin>>matrix[i][j];
-			
-	sqrtMethodSolver* sms=new sqrtMethodSolver();	
+	
+	sqrtMethodSolver* sms=new sqrtMethodSolver();
+	time_before=time(NULL);	
 	double* x=sms->getSolve(matrix, size);
+	time_after=time(NULL);
+	cout << "time: " << time_after-time_before << endl;
 	for(int j=0; j<size; j++)
 		cout << x[j] << "\t";
 	cout << endl << "----------------------" << endl;
 	
-	conjugateGradientSolver* cgs=new conjugateGradientSolver();
+	/*conjugateGradientSolver* cgs=new conjugateGradientSolver();
 	cgs->setAccuracy(0.000001);
 	x=cgs->getSolve(matrix, size);
 	for(int j=0; j<size; j++)
 		cout << x[j] << "\t";
-	cout << endl << "----------------------" << endl;
+	cout << endl << "----------------------" << endl;*/
 	
     return 0;
 }
